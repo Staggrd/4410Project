f22675cea4594ba50aea8af9d47e129e65b97ee1
Staggrd <wds3r@mtmail.mtsu.edu>
2025-04-23 19:50:40 -0500

    Done with Admin functions for now
    Could not complete every function under the Admin table but got most of them for now. Also, Frontend please link up a CSS file to this page.

--- CSCI4410Project/dashboardAdmin.php
+++ CSCI4410Project/dashboardAdmin.php
@@ -7,7 +7,7 @@
         exit;
     }
     echo "<h2>Welcome! You are logged in to your administrator account.</h2>";
-    echo "<a href='logout.php'>Logout</a><br>"; //this will be our logout hyperlink (added break for spacing)
+    echo "<a href='logout.php'>Logout</a><br><br>"; //this will be our logout hyperlink (added break for spacing)
 
     //creating a reusable function for displaying a table
 function displayTable($result){
@@ -33,16 +33,16 @@ function displayTable($result){
     <button name="display_booklist">Get Booklist</button> <!-- Added Function -->
     <button name="add_book">Add Book to List</button> <!-- Added Function -->
     <button name="delete_book">Delete Book from List</button> <!-- Added Function-->
-    <button name="update_book">Update Book Info</button> <!-- -->
+    <button name="update_book">Update Book Info</button> <!-- Added Function -->
     <button name="display_users">Get Userlist</button> <!-- Added Function -->
     <button name="add_user">Add User</button> <!-- Added Function -->
-    <button name="delete_user">Delete User</button> <!-- -->
+    <button name="delete_user">Delete User</button> <!-- Added Function-->
 </form>
 
 <?php //now we will give all these buttons functionality
 
 if (isset($_POST['display_booklist'])) { //IF FUNCTION FOR DISPLAYINGA ALL BOOKLIST WITHOUT IMAGE
-    $sql = "SELECT book_id, title, author, ISBN, category, total_copies, available copies FROM books"; //save query to $sql
+    $sql = "SELECT * FROM books"; //save query to $sql
     $result = $conn->query($sql);
     // Call the function to display table
      displayTable($result);
@@ -64,23 +64,23 @@ if (isset($_POST['add_book'])) { //first we display a form for entering new book
     echo 'Image URL: <input type="text" name="image_url" required><br>';
     echo '<button type="submit" name="add_book_submit">Add Book</button>';
     echo '</form><br>';
+}
 
-    if isset($_POST['add_book_submit'])) { //now we will put information into the 'books' table
-        $title = $_POST["title"];
-        $author = $_POST["author"];
-        $ISBN = $_POST["ISBN"];
-        $category = $_POST["category"];
-        $image_url = $_POST["image_url"];
+if (isset($_POST['add_book_submit'])) { //now we will put information into the 'books' table
+    $title = $_POST["title"];
+    $author = $_POST["author"];
+    $ISBN = $_POST["ISBN"];
+    $category = $_POST["category"];
+    $image_url = $_POST["image_url"];
 
-        $stmt = $conn->prepare("INSERT INTO books (title, author, ISBN, category, image_url, total_copies, available_copies) VALUES (?, ?, ?, ?, ?, 1, 1)");
-        $stmt->bind_param("sssss", $title, $author, $ISBN, $category, $image_url);
+    $stmt = $conn->prepare("INSERT INTO books (title, author, ISBN, category, image_url, total_copies, available_copies) VALUES (?, ?, ?, ?, ?, 1, 1)");
+    $stmt->bind_param("sssss", $title, $author, $ISBN, $category, $image_url);
 
-        try{
-            $stmt->execute();
-            echo "Book added successfully!";
-        } catch (mysqli_sql_exception $e){
-            echo "Book already exists in database.";
-        }
+    try{
+        $stmt->execute();
+        echo "Book added successfully!";
+    } catch (mysqli_sql_exception $e){
+        echo "Book already exists in database.";
     }
 }
 
@@ -90,18 +90,20 @@ if (isset($_POST['delete_book'])) { //we first create a form for the book to be 
     echo '<button type="submit" name="delete_book_submit">Delete Book</button>';
     echo '</form><br>';
 
-    if isset($_POST['delete_book_submit'])) {//then once submitted, we prepare a statement and 
-        $title = $_POST["title"];
+    
+}
 
-        $stmt = $conn->prepare("DELETE FROM books WHERE title='?'");
-        $stmt->bind_param("s", $title);
+if (isset($_POST['delete_book_submit'])) {//then once submitted, we prepare a statement and 
+    $title = $_POST["title"];
 
-        try{
-            $stmt->execute();
-            echo "Book deleted successfully!";
-        } catch (mysqli_sql_exception $e){
-            echo "Something went wrong.";
-        }
+    $stmt = $conn->prepare("DELETE FROM books WHERE title=?");
+    $stmt->bind_param("s", $title);
+
+    try{
+        $stmt->execute();
+        echo "Book deleted successfully!";
+    } catch (mysqli_sql_exception $e){
+        echo "Something went wrong.";
     }
 }
 
@@ -115,29 +117,78 @@ if (isset($_POST['add_user'])) { //first we display a form for entering new user
         echo '<option value="Student">Student</option>';
         echo '<option value="Faculty">Faculty</option>';
         echo '<option value="Admin">Admin</option>';
-    echo '<button type="submit" name="add_user_submit">Add Book</button>';
+    echo '</select><br>';
+    echo '<button type="submit" name="add_user_submit">Add User</button>';
     echo '</form><br>';
+}
 
-    if isset($_POST['add_user_submit'])) { //now we will put information into the 'users' table
-        $username = $_POST["username"]; //username
-        $password = password_hash($_POST["password"], PASSWORD_DEFAULT); //password
-        $email = $_POST["email"]; //email
-        $role = $_POST["role"]; //role
-        $phone = $_POST["phone"]; //phone
+if (isset($_POST['add_user_submit'])) { //now we will put information into the 'users' table
+    $username = $_POST["username"]; //username
+    $password = password_hash($_POST["password"], PASSWORD_DEFAULT); //password
+    $email = $_POST["email"]; //email
+    $role = $_POST["role"]; //role
+    $phone = $_POST["phone"]; //phone
 
-        $stmt = $conn->prepare("INSERT INTO users (username, password, email, phone, role) VALUES (?, ?, ?, ?, ?)");
-        $stmt->bind_param("sssss", $username, $password, $email, $phone, $role);
+    $stmt = $conn->prepare("INSERT INTO users (username, password, email, phone, role) VALUES (?, ?, ?, ?, ?)");
+    $stmt->bind_param("sssss", $username, $password, $email, $phone, $role);
 
-        try{
-            $stmt->execute();
-            echo "User added successfully!";
-        } catch (mysqli_sql_exception $e){
-            echo "User already exists in database.";
-        }
+    try{
+        $stmt->execute();
+        echo "User added successfully!";
+    } catch (mysqli_sql_exception $e){
+        echo "User already exists in database.";
     }
 }
 
+if (isset($_POST['delete_user'])) { //first we display a form for entering new user information
+    echo '<form method="post">';
+    echo 'Username of account to delete: <input name="username" required><br>';
+    echo '<button type="submit" name="delete_user_submit">Add Book</button>';
+    echo '</form><br>';
+}
 
+if (isset($_POST['delete_user_submit'])) { //now we execute the previous form
+    $username = $_POST["username"];
 
+    $stmt = $conn->prepare("DELETE FROM users WHERE username=?");
+    $stmt->bind_param("s", $username);
 
+    try{
+        $stmt->execute(); //try to execute statement
+        echo "Deleted successfully!"; //Display if successful
+    } catch (mysqli_sql_exception $e){
+        echo "Something went wrong."; //Display if failed
+    }
+}
+
+if (isset($_POST['update_book'])) { //show a form to update a book
+    echo '<form method="post">';
+    echo 'BookID to Update: <input type="number" name="book_id" required><br>';
+    echo 'Title: <input type="text" name="title" required><br>';
+    echo 'Author: <input type="text" name="author" required><br>';
+    echo 'ISBN: <input type="text" name="ISBN" required><br>';
+    echo 'Category: <input type="text" name="category" required><br>';
+    echo 'Image URL: <input type="text" name="image_url" required><br>';
+    echo '<button type="submit" name="update_book_submit">Update Book</button>';
+    echo '</form><br>';
+}
+
+if (isset($_POST['update_book_submit'])) { //now we execute the previous form
+    $book_id = $_POST["book_id"];
+    $title = $_POST["title"];
+    $author = $_POST["author"];
+    $ISBN = $_POST["ISBN"];
+    $category = $_POST["category"];
+    $image_url = $_POST["image_url"];
+
+    $stmt = $conn->prepare("UPDATE books SET title=?, author=?, ISBN=?, category=?, image_url=? WHERE book_id=?");
+    $stmt->bind_param("sssssi", $title, $author, $ISBN, $category, $image_url, $book_id);
+
+    try{
+        $stmt->execute(); //try to execute statement
+        echo "Updated successfully!"; //Display if successful
+    } catch (mysqli_sql_exception $e){
+        echo "Something went wrong."; //Display if failed
+    }
+}
 ?>
